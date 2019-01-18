42c5552d8187f775e88dc9afaa81ae0c79040695
dakota-osu <altond@oregonstate.edu>
2019-01-17 20:03:02 -0800

    adds ShipTest test suite file

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -7,18 +7,42 @@ import java.util.List;
 
 public class Ship {
 
-	@JsonProperty private List<Square> occupiedSquares;
+	public static enum ShipType {
+		MINESWEEPER(2),
+		BATTLESHIP(3),
+		DESTOYER(4),
+		INVALID(0);
+
+		private final int value;
+
+		private ShipType(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return this.value;
+		}
+	}
+
+	@JsonProperty 
+	private List<Square> occupiedSquares;
+
+	private ShipType shipType;
 
 	public Ship() {
-		occupiedSquares = new ArrayList<>();
+		this("INVALID");
 	}
 	
 	public Ship(String kind) {
-		//TODO implement
+		this.occupiedSquares = new ArrayList<Square>();
+		this.shipType = ShipType.valueOf(kind);
+	}
+
+	public ShipType getShipType() {
+		return this.shipType;
 	}
 
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
 		return null;
 	}
 }
---
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -0,0 +1,22 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class ShipTest {
+
+    @Test
+    public void ShipsHaveTheCorrectType() {
+        fail();
+    }
+
+    @Test
+    public void ShipsHandleJunkType() {
+        fail();
+    } 
+
+
+}
